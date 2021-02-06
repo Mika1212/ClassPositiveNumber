77ed57e80a54896882d7b072f4ded49e24970c84
Mika1212 <54846851+Mika1212@users.noreply.github.com>
2021-02-06 21:57:42 +0300

    New Class

--- class/src/PositiveNumber.java
+++ class/src/PositiveNumber.java
@@ -6,6 +6,10 @@ public class PositiveNumber implements Comparable<PositiveNumber> {
 
     ArrayList<Integer> number;
 
+    public static final PositiveNumber ZERO = new PositiveNumber(0);
+    public static final PositiveNumber ONE = new PositiveNumber(1);
+    public static final PositiveNumber TEN = new PositiveNumber(10);
+
     public  PositiveNumber(){
         this.number = new ArrayList<>();
     }
@@ -89,74 +93,7 @@ public class PositiveNumber implements Comparable<PositiveNumber> {
         return res;
     }
 
-    public PositiveNumber minus(int other) {
-        ArrayList<Integer> result = new ArrayList<>();
-        ArrayList<Integer> smth = arrayMaker(other);
-        if (smth.size()>number.size() ||
-                (smth.size() == number.size() && smth.get(smth.size()-1) > number.get(smth.size()-1))) {
-            System.out.println('1');
-            return null;
-        }
 
-        for (int i = 0; i< smth.size(); i++) {
-            result.add(number.get(i) - smth.get(i));
-        }
-
-        for (int i = smth.size(); i<number.size();i++){
-            result.add(number.get(i));
-        }
-
-        result = transformation(result);
-        PositiveNumber res = new PositiveNumber();
-        res.number = result;
-        return res;
-    }
-
-    public PositiveNumber minus(BigInteger other) {
-        ArrayList<Integer> result = new ArrayList<>();
-        ArrayList<Integer> smth = arrayMaker(other);
-        if (smth.size()>number.size() ||
-                (smth.size() == number.size() && smth.get(smth.size()-1) > number.get(smth.size()-1))) {
-            System.out.println('1');
-            return null;
-        }
-
-        for (int i = 0; i< smth.size(); i++) {
-            result.add(number.get(i) - smth.get(i));
-        }
-
-        for (int i = smth.size(); i<number.size();i++){
-            result.add(number.get(i));
-        }
-
-        result = transformation(result);
-        PositiveNumber res = new PositiveNumber();
-        res.number = result;
-        return res;
-    }
-
-    public PositiveNumber minus(PositiveNumber other) {
-        ArrayList<Integer> result = new ArrayList<>();
-        ArrayList<Integer> smth = other.number;
-        if (smth.size()>number.size() ||
-                (smth.size() == number.size() && smth.get(smth.size()-1) > number.get(smth.size()-1))) {
-            System.out.println("first");
-            return null;
-        }
-
-        for (int i = 0; i< smth.size(); i++) {
-            result.add(number.get(i) - smth.get(i));
-        }
-
-        for (int i = smth.size(); i<number.size();i++){
-            result.add(number.get(i));
-        }
-
-        result = transformation(result);
-        PositiveNumber res = new PositiveNumber();
-        res.number = result;
-        return res;
-    }
 
     public PositiveNumber remainderOfDivision(int other) {
         PositiveNumber result = new PositiveNumber();
@@ -197,23 +134,16 @@ public class PositiveNumber implements Comparable<PositiveNumber> {
         return result;
     }
 
-    public PositiveNumber division(PositiveNumber other) {
-        PositiveNumber result = new PositiveNumber();
-        result.number.addAll(number);
-        ArrayList<Integer> some = factoring(other);
-        for (Integer integer : some) {
-            result = result.division(integer);
-        }
 
-        result.number = transformation(result.number);
-        return result;
-    }
 
+    /*
     public PositiveNumber division(BigInteger other) {
         PositiveNumber some = new PositiveNumber();
         some.number = arrayMaker(other);
         return this.division(some);
     }
+
+     */
 
     public static ArrayList<Integer> factoring(PositiveNumber some) {
         ArrayList<Integer> result = new ArrayList<>();
@@ -277,12 +207,13 @@ public class PositiveNumber implements Comparable<PositiveNumber> {
     @Override
     public int compareTo(PositiveNumber o) {
 
-        if (this.minus(o) == null) return -1;
+        if (this.minus(o).number.toString().equals(new PositiveNumber().number.toString())) return -1;
         else if
             (this.minus(o).number.toString().equals(new PositiveNumber(0).number.toString()))
                 return 0;
         else return 1;
     }
+
 
     public int compareTo(int o) {
 
@@ -300,17 +231,23 @@ public class PositiveNumber implements Comparable<PositiveNumber> {
             if (array.get(i)>=10) {
                 if (array.size()<i+2) {
                     array.add(array.get(i) / 10);
-                } else
-                    array.set(i+1, array.get(i+1) + (array.get(i) / 10));
-                array.set(i, array.get(i) % 10);
-            }
-            if  (array.get(i)<0) {
-                if (array.size()<i+2) {
-                    System.out.println("second");
-                    return null;
+                    array.set(i, array.get(i) % 10);
+                } else {
+                    array.set(i + 1, array.get(i + 1) + (array.get(i) / 10));
+                    array.set(i, array.get(i) % 10);
                 }
-                array.set(i+1, (array.get(i+1) / 10)*-1);
-                array.set(i, (array.get(i) % 10)*-1);
+            } else {
+                if (array.get(i) < 0) {
+                    if (array.size() < i + 2) {
+                        System.out.println("second");
+                        return new ArrayList<>();
+                    }
+                    while (array.get(i)<0) {
+                        array.set(i, array.get(i)+10);
+                        array.set(i+1, array.get(i+1) - 1);
+                    }
+
+                }
             }
         }
         while (array.size()>1 && array.get(array.size()-1) == 0) {
@@ -329,17 +266,129 @@ public class PositiveNumber implements Comparable<PositiveNumber> {
         return result.toString();
     }
 
+    public ArrayList<Integer> reverse() {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = number.size()-1; i>=0; i--) {
+            result.add(number.get(i));
+        }
+        return result;
+    }
+
+    public PositiveNumber minus(PositiveNumber other) {
+        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<Integer> smth = other.number;
+        if (smth.size()>number.size())
+            return new PositiveNumber();
+
+        for (int i = 0; i< smth.size(); i++) {
+            result.add(number.get(i) - smth.get(i));
+        }
+
+        for (int i = smth.size(); i<number.size();i++){
+            result.add(number.get(i));
+        }
+
+        result = transformation(result);
+        PositiveNumber res = new PositiveNumber();
+        res.number = result;
+        return res;
+    }
+
+    public PositiveNumber clone(){
+        PositiveNumber result = new PositiveNumber();
+        for (int i = 0; i < this.number.size()-1; i++) {
+            result.number.add(this.number.get(i));
+        }
+        return result;
+    }
+
+    public void zeroDelete(){
+        while (this.number.size()>1 && this.number.get(this.number.size()-1) == 0) {
+            this.number.remove(this.number.size() - 1);
+        }
+    }
+
+
+    public PositiveNumber division(PositiveNumber other) {
+        PositiveNumber result = new PositiveNumber();
+        PositiveNumber divided = new PositiveNumber();
+        divided.number.addAll(this.number);
+        result.number.add(0);
+        PositiveNumber helper = new PositiveNumber();
+        if (this.compareTo(other) < 0) return new PositiveNumber(0);
+        int o =0;
+
+
+        while (divided.compareTo(PositiveNumber.ZERO) > 0
+                || divided.number.size()+helper.number.size() > other.number.size()) {
+
+            System.out.println("r with mult " + result);
+            result = result.multiplication(10);
+
+            for (int i = divided.number.size() - 1; i >= 0; i--) {
+                helper.number.add(0, divided.number.get(i));
+                helper.zeroDelete();
+
+                System.out.println("                                               Helper now = " + helper.number);
+                if (helper.compareTo(other) >= 0) {
+                    System.out.println("entrance " + helper.number);
+                    while (helper.compareTo(other) >= 0) {
+                        result = result.plus(PositiveNumber.ONE);
+                        helper = helper.minus(other);
+                    }
+                    int k = divided.number.size() - i;
+                    for (int j = 0; j < k; j++) {
+                        divided.number.remove(divided.number.size() - 1);
+                    }
+
+                    System.out.println("divided = " + divided + " helper = " + helper.number);
+                    System.out.println(divided.number + " f f " + helper.number);
+                    System.out.println("result = " + result);
+
+                    break;
+                }
+
+                if (divided.number.size() != number.size() && divided.number.size() - i > 1) {
+                    result = result.multiplication(10);
+                }
+                System.out.println("result = " + result);
+
+                if (i == 0) return result;
+            }
+
+            int k = divided.number.size();
+            if (PositiveNumber.ONE.compareTo(helper) > 0) {
+                PositiveNumber test = divided.clone();
+                test.zeroDelete();
+                if (PositiveNumber.ONE.compareTo(test) > 0) {
+                for (int i = 0; i<k; i++ ) {
+                    System.out.println("success");
+                    result = result.multiplication(10);
+
+                   }
+                break;
+                }
+            }
+            System.out.println("------------------------");
+            o++;
+        }
+
+
+        return result;
+    }
+
 }
 
 class Main{
     public static void main(String[] args) {
-
-        PositiveNumber test = new PositiveNumber("1234567899");
-
+        PositiveNumber x = new PositiveNumber("1219028852");
+        PositiveNumber test = new PositiveNumber( "1234567899");
         PositiveNumber test1 = new PositiveNumber("54654165498265456546523154486548797886798451654");
 
-        System.out.println(test1.division(100).number);
+        PositiveNumber a = new PositiveNumber("100000000");
+        PositiveNumber b = new PositiveNumber(600);
 
-        System.out.println(test1.toString());
+        System.out.println(a.division(b));
+
     }
 }
